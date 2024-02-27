4881acb23273d68f44beb94264062b505f6f7d0a
Santiago-Balcero <santiagobalcero@gmail.com>
2024-02-27 17:42:17 -0500

    feat: shut down log

--- bot/bot.go
+++ bot/bot.go
@@ -39,13 +39,15 @@ func Run() {
 	discord.AddHandler(newMessage)
 
 	// open session
-	discord.Open()
+	err = discord.Open()
+	checkErr(err)
 	defer discord.Close()
 
 	log.Println("Bot running...")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
+	log.Println("Bot shut down")
 }
 
 func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
