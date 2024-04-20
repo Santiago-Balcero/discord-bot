e975dcb6dce0fbf1f77aad3d3213dea6d37c4707
Santiago-Balcero <santiagobalcero@gmail.com>
2024-04-20 13:20:30 -0500

    fix: :green_heart: .env vars for Docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,22 @@
 FROM golang:1.22.0
 
+RUN useradd -m -s /bin/bash appuser
+USER appuser
+
 WORKDIR /usr/src/app
 
 COPY go.mod go.sum ./
-
 RUN go mod download && go mod verify
 
 COPY . .
-
+RUN git config --global --add safe.directory /usr/src/app
+RUN if [ -f .env ]; then \
+    while IFS= read -r line || [[ -n "$line" ]]; do \
+        export "$line"; \
+    done < .env; \
+    fi
 RUN go build -v -o /usr/src/app ./...
+
+RUN rm -rf go.mod go.sum *.go .env .gitignore .todo *.md
 
 CMD ["/usr/src/app/discord-spotify-bot"]
--- config/env.go
+++ config/env.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"os"
 
-	"discord-spotify-bot/utils"
-
 	"github.com/joho/godotenv"
 )
 
@@ -15,11 +13,13 @@ var BotToken string
 var DiscordAppId string
 
 func LoadConfig() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env: ", err)
+	if dockerEnv := os.Getenv("DOCKER_ENV"); dockerEnv == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env: ", err)
+		}
 	}
-	utils.CheckError(err)
+
 	SpotifyId = os.Getenv("SPOTIFY_ID")
 	SpotifySecret = os.Getenv("SPOTIFY_KEY")
 	BotToken = os.Getenv("BOT_TOKEN")
