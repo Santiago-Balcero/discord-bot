bbe0d9200a3353176abfb6f419dc27f0231a02e8
Santiago-Balcero <santiagobalcero@gmail.com>
2024-04-20 18:12:49 -0500

    fix: handles if no features come with track

--- services/track.go
+++ services/track.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"discord-spotify-bot/models"
 
@@ -15,14 +16,17 @@ func AnalyseTrack(client *spotify.Client, track *models.Track) error {
 	if err != nil {
 		return fmt.Errorf("error in GetAudioFeatures: %v", err)
 	}
+	if features[0] == nil {
+		log.Println("No features found for track:", track.Name)
+		return nil
+	}
 
 	track.Danceability = features[0].Danceability
 	track.Energy = features[0].Energy
 	track.Acousticness = features[0].Acousticness
 	track.Loudness = features[0].Loudness
 	track.Liveness = features[0].Liveness
-	track.Instrumentalness = features[0].Instrumentalness
 	track.Liveness = features[0].Tempo
-
+	track.Instrumentalness = features[0].Instrumentalness
 	return nil
 }
